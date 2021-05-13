be667237ca6fc7e278b1172b22eaac5cac295076
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2021-05-13 18:35:29 -0300

    linting

--- models/user.js
+++ models/user.js
@@ -1,53 +1,54 @@
 const mongoose = require('mongoose')
+const process = require('process')
 
 const url = process.env.MONGODB_URI
 
 console.log(`connecting to ${url}`)
 
 mongoose.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
 
 }).then(() => {
-    console.log('connected to MongoDB')
+  console.log('connected to MongoDB')
 
 }).catch((error) => {
-    console.log(`error connecting to MongoDB: ${error.message}`)
+  console.log(`error connecting to MongoDB: ${error.message}`)
 
 })
 
 const userSchema = new mongoose.Schema({
-    first_name: {
-        type: String,
-        required: true
-    },
-    last_name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    age: {
-        type: Number,
-        required: true
-    }
+  first_name: {
+    type: String,
+    required: true
+  },
+  last_name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true
+  }
 })
 
 userSchema.set('toJSON', {
-    transform: (document, object) => {
-        object.id = object._id.toString()
-        delete object.password
-        delete object._id
-        delete object.__v
-    }
+  transform: (document, object) => {
+    object.id = object._id.toString()
+    delete object.password
+    delete object._id
+    delete object.__v
+  }
 })
 
 module.exports = mongoose.model('User', userSchema)
