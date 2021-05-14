6cc6dcf60f1e1f78027a139485569cc8b5dfbe62
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2021-05-14 14:43:03 -0300

    added comments

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const http = require('http')
+const http = require('http')    // used to create the web-server
 
-const app = require('./app')
-const config = require('./utils/config')
-const logger = require('./utils/logger')
+const app = require('./app')              // express application
+const config = require('./utils/config')  // used to get the port for the server
+const logger = require('./utils/logger')  // logs the server status
 
-const server = http.createServer(app)
-
-server.listen(config.PORT, () => {
+/**
+ * Creates the web server
+ */
+http.createServer(app).listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
