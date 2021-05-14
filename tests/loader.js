f63e9daa7033ed66d5ce8f4d9af872e34ca79604
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2021-05-14 17:14:29 -0300

    created loader

--- tests/loader.js
+++ tests/loader.js
@@ -1,2 +1,39 @@
 const fs = require('fs')
+const mongoose = require('mongoose')
+const User = require('../models/user')
+const bcrypt = require('bcrypt')
+const process = require('process')
 
+const data = JSON.parse(fs.readFileSync('./data/mock_data.json'))
+
+/**
+ * connects to the database specified by config
+ */
+mongoose.connect(process.argv[2], {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+})
+  .then(() => {
+    console.log('connected to MongoDB')
+
+    data.map(async (user) => {
+
+      const passwordHash = await bcrypt.hash(user.password, 1)
+
+      const userObject = new User({
+        first_name: user.first_name,
+        last_name: user.last_name,
+        email: user.email,
+        password_hash: passwordHash,
+        age: user.age
+      })
+      const savedUser = await userObject.save()
+      console.log(savedUser)
+    })
+
+  })
+  .catch((error) => {
+    console.error('error connecting to MongoDB', error.message)
+  })
