7c5c52b10b3076aa9ec1ccb2c20aafe76ca1fc24
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2021-05-13 23:25:38 -0300

    improved documentation

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
-require('express-async-errors')
+require('express-async-errors')   // used to handle exceptions by automatically calling next
 
-const cors = require('cors')
+const cors = require('cors')  // used for cross-origin requests
 
-const config = require('./utils/config')
-const usersRouter = require('./controllers/users')
-const middleware = require('./utils/middleware')
-const logger = require('./utils/logger')
+const config = require('./utils/config')            // loads the config for the server (create a .env)
+const usersRouter = require('./controllers/users')  // routes related to user operation
+const middleware = require('./utils/middleware')    // middlewares used by the application
+const logger = require('./utils/logger')            // logger used by the application
 
-const mongoose = require('mongoose')
-const express = require('express')
+const mongoose = require('mongoose')  // high level package for operation on MongoDB
+const express = require('express')    // express server
 const app = express()
 
+// == Connecting to the database ==================================================================================== //
 
 logger.info(`connecting to ${config.MONGODB_URI}`)
 
+/**
+ * connects to the database specified by config
+ */
 mongoose.connect(config.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -27,16 +31,36 @@ mongoose.connect(config.MONGODB_URI, {
     logger.error('error connecting to MongoDB', error.message)
   })
 
+// == Express server ================================================================================================ //
+
+/**
+ * Adds support for cross origin requests
+ */
 app.use(cors())
 
+/**
+ * JSON parser
+ */
 app.use(express.json())
 
+/**
+ * Logs requests sent to the server
+ */
 app.use(middleware.requestLogger)
 
+/**
+ * Routes related to user
+ */
 app.use('/api/users', usersRouter)
 
+/**
+ * Handles unknown endpoints
+ */
 app.use(middleware.unknownEndpoint)
 
+/**
+ * handles errors
+ */
 app.use(middleware.errorHandler)
 
 module.exports = app
