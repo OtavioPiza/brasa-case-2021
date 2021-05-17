2f38f683ae5fa97ff91bc1253447eb513b452409
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2021-05-17 15:41:12 -0300

    added tests

--- tests/user_api.test.js
+++ tests/user_api.test.js
@@ -4,74 +4,24 @@ const helper = require('./test_helper')   // helper functions
 
 const api = require('supertest')(require('../app'))
 
-describe('Three initial users in the database', () => {
-
-  /**
-   * Prepares the db for testing
-   */
-  beforeEach(async () => {
-    await User.deleteMany({})
-    const userObjects = helper.testUsers.map(user => {
-      return new User({
-        first_name: user.first_name,
-        last_name: user.last_name,
-        email: user.email,
-        password_hash: user.password_hash,
-        age: user.age
-      })
+/**
+ * Prepares the db for testing
+ */
+beforeEach(async () => {
+  await User.deleteMany({})
+  const userObjects = helper.testUsers.map(user => {
+    return new User({
+      first_name: user.first_name,
+      last_name: user.last_name,
+      email: user.email,
+      password_hash: user.password_hash,
+      age: user.age
     })
-    await Promise.all(userObjects.map(user => user.save()))
   })
+  await Promise.all(userObjects.map(user => user.save()))
+})
 
-  /**
-   * Tests the creation of a valid user
-   */
-  test('valid new user', async () => {
-    const initialUsers = await helper.usersInDb()
-    const newUser = {
-      password: 'password',
-      first_name: 'new',
-      last_name: 'new',
-      email: 'new@email.com',
-      age: 1
-    }
-    await api
-      .post('/api/users')
-      .send(newUser)
-      .expect(200)
-      .expect('Content-Type', /application\/json/)
-
-    const finalUsers = await helper.usersInDb()
-    const emails = finalUsers.map(user => user.email)
-    expect(finalUsers).toHaveLength(initialUsers.length + 1)
-    expect(emails).toContain('new@email.com')
-  })
-
-  /**
-   * Test the creation of an invalid user
-   */
-  test('creation fails with a duplicated email', async () => {
-    const initialUsers = await helper.usersInDb()
-
-    const newUser = {
-      email: helper.testUsers[0].email,
-      password: helper.testUsers[0].password,
-      first_name: 'fail',
-      last_name: 'fail',
-      age: -1
-    }
-
-    const result = await api
-      .post('/api/users')
-      .send(newUser)
-      .expect(400)
-      .expect('Content-Type', /application\/json/)
-
-    const finalUsers = await helper.usersInDb()
-
-    expect(result.body.error).toContain('`email` to be unique')
-    expect(finalUsers).toHaveLength(initialUsers.length)
-  })
+describe('Viewing users', () => {
 
   /**
    * Test getting all users
@@ -111,19 +61,22 @@ describe('Three initial users in the database', () => {
       .get(`/api/users/${helper.testUsers[0].email.concat('invalid')}`)
       .expect(404)
   })
+})
+
+describe('Logging in users', () => {
 
   /**
    * Valid login
    */
   test('valid login', async () => {
     const result = await api
-      .post('/api/login')
-      .send({
-        email: helper.testUsers[0].email,
-        password: helper.testUsers[0].password
-      })
-      .expect(200)
-      .expect('Content-Type', /application\/json/)
+        .post('/api/login')
+        .send({
+          email: helper.testUsers[0].email,
+          password: helper.testUsers[0].password
+        })
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
 
     expect(result.body.token !== undefined)
   })
@@ -133,15 +86,68 @@ describe('Three initial users in the database', () => {
    */
   test('invalid login', async () => {
     const result = await api
-      .post('/api/login')
-      .send({
-        email: helper.testUsers[0].email,
-        password: helper.testUsers[0].password.concat('invalid')
-      })
-      .expect(401)
-      .expect('Content-Type', /application\/json/)
+        .post('/api/login')
+        .send({
+          email: helper.testUsers[0].email,
+          password: helper.testUsers[0].password.concat('invalid')
+        })
+        .expect(401)
+        .expect('Content-Type', /application\/json/)
 
     expect(result.body.error).toContain('invalid')
+  })
+})
+
+describe('Adding users', () => {
+
+  /**
+   * Tests the creation of a valid user
+   */
+  test('creating succeeds with valid new user', async () => {
+    const initialUsers = await helper.usersInDb()
+    const newUser = {
+      password: 'password',
+      first_name: 'new',
+      last_name: 'new',
+      email: 'new@email.com',
+      age: 1
+    }
+    await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+    const finalUsers = await helper.usersInDb()
+    const emails = finalUsers.map(user => user.email)
+    expect(finalUsers).toHaveLength(initialUsers.length + 1)
+    expect(emails).toContain('new@email.com')
+  })
+
+  /**
+   * Test the creation of an invalid user
+   */
+  test('creation fails with a duplicated email', async () => {
+    const initialUsers = await helper.usersInDb()
+
+    const newUser = {
+      email: helper.testUsers[0].email,
+      password: helper.testUsers[0].password,
+      first_name: 'fail',
+      last_name: 'fail',
+      age: -1
+    }
+
+    const result = await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
+
+    const finalUsers = await helper.usersInDb()
+
+    expect(result.body.error).toContain('`email` to be unique')
+    expect(finalUsers).toHaveLength(initialUsers.length)
   })
 })
 
